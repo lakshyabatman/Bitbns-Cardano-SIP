add7f4167df74c086f61d56249fd488550ddcf9d
lakshyabatman <lakshya.khera@gmail.com>
2021-09-18 14:37:47 +0530

    feat: added support for crypto list

--- src/index.js
+++ src/index.js
@@ -18,13 +18,24 @@ app.get("/",(req,res) => {
     return res.json({message:'Hello world'})
 })
 
+
+const runner = (cryptos) => {
+    let cryptoArray = String(cryptos).split(",")
+    for(let crypto of cryptoArray) {
+        console.log(crypto)
+        buyCrypto(crypto,process.env.currency,parseFloat(process.env.amount)) 
+    }
+
+}
+
 app.post("/", (req,res) => {
     if(req.headers['vercel_secret'] === process.env.VERCEL_SECRET) {
-        buyCrypto(process.env.crypto,process.env.currency,parseFloat(process.env.amount))
+        runner(process.env.crypto)
         return res.status(200).json({message:'Success'})
     }
     return res.status(401).json({message: 'Unauth'})
 })
+
 
 app.listen(process.env.PORT || 3000 ,() => {
     console.log("Server running")
